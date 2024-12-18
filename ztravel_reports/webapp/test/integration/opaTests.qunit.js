3e3dac5279b522056359a33cc1cd2b3d05e32ec1
JobmakerRaghava <123930030+JobmakerRaghava@users.noreply.github.com>
2024-12-18 19:07:26 +0530

    Delete ztravel_reports directory

--- ztravel_reports/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["com/taqa/travelreports/ztravelreports/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- ztravel_reports/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"comtaqatravelreports/ztravel_reports/test/unit/controller/ReportsView.controller"
-], function () {
-	"use strict";
-});
